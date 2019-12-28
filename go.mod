a6474167a7623b74e43d06b3916c3be67ab0df92
Gabriel Sanches <gabriel@gsr.dev>
2019-12-28 20:07:48 -0300

    install gopls
    This is intended to control the version of gopls, a Go LSP server.
    
    By controlling its version, it is possible to have reproducible
    development environments with working autocompletion.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gsr.dev/pilgrim
 
 go 1.13
+
+require golang.org/x/tools/gopls v0.2.2
---
+++ tools/tools.go
@@ -0,0 +1,9 @@
+// +build tools
+
+package tools
+
+import (
+	// The following imports are tools intended
+	// to be used while developing or running CI.
+	_ "golang.org/x/tools/gopls"
+)
