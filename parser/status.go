e6b9b41b28c6c2d61fe481476e06de81600cb2a3
Gabriel Sanches <gabriel@gsr.dev>
2020-01-02 17:45:19 -0300

    parser: add method to return status as text

--- parser/status.go
+++ parser/status.go
@@ -1,5 +1,7 @@
 package parser
 
+import "strings"
+
 // Status is a node's status.
 type Status uint8
 
@@ -7,3 +9,14 @@ const (
 	// StatusReady means the node is ready to be symlinked.
 	StatusReady Status = iota + 1
 )
+
+func (s Status) String() string { return strings.ToUpper(s.str()) }
+
+func (s Status) str() string {
+	switch s {
+	case StatusReady:
+		return "ready"
+	default:
+		return "undefined"
+	}
+}
