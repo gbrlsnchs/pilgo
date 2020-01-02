ebb3f9e1375ea0e06aea69033144255f3fa682fe
Gabriel Sanches <gabriel@gsr.dev>
2020-01-02 17:58:20 -0300

    parser: add method to return file full path

--- parser/file.go
+++ parser/file.go
@@ -1,7 +1,18 @@
 package parser
 
+import "path/filepath"
+
 // File holds a file's metadata.
 type File struct {
 	BaseDir string
 	Path    []string
+}
+
+// FullPath joins base directory with all path segments.
+func (f File) FullPath() string {
+	fullPath := append(
+		make([]string, 0, len(f.Path)+1),
+		f.BaseDir,
+	)
+	return filepath.Join(append(fullPath, f.Path...)...)
 }
---
+++ parser/file_test.go
@@ -0,0 +1,52 @@
+package parser_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	"gsr.dev/pilgrim/parser"
+)
+
+func TestFile(t *testing.T) {
+	t.Run("FullPath", testFileFullPath)
+}
+
+func testFileFullPath(t *testing.T) {
+	testCases := []struct {
+		f    parser.File
+		want string
+	}{
+		{
+			f:    parser.File{},
+			want: "",
+		},
+		{
+			f: parser.File{
+				BaseDir: "test",
+				Path:    nil,
+			},
+			want: "test",
+		},
+		{
+			f: parser.File{
+				BaseDir: "test",
+				Path:    []string{"foo"},
+			},
+			want: filepath.Join("test", "foo"),
+		},
+		{
+			f: parser.File{
+				BaseDir: "test",
+				Path:    []string{"foo", "bar"},
+			},
+			want: filepath.Join("test", "foo", "bar"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.want, func(t *testing.T) {
+			if want, got := tc.want, tc.f.FullPath(); got != want {
+				t.Errorf("want %q, got %q", want, got)
+			}
+		})
+	}
+}
