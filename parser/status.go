0f7ca404fadaeb2f8d87eece0bf12b1becef98ec
Gabriel Sanches <gabriel@gsr.dev>
2020-01-02 18:17:23 -0300

    parser: add status "skip"

--- parser/status.go
+++ parser/status.go
@@ -8,6 +8,8 @@ type Status uint8
 const (
 	// StatusReady means the node is ready to be symlinked.
 	StatusReady Status = iota + 1
+	// StatusSkip means the node has children and thus might be skipped.
+	StatusSkip
 )
 
 func (s Status) String() string { return strings.ToUpper(s.str()) }
@@ -16,6 +18,8 @@ func (s Status) str() string {
 	switch s {
 	case StatusReady:
 		return "ready"
+	case StatusSkip:
+		return "skip"
 	default:
 		return "undefined"
 	}
