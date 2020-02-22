bffda62d751d527c9292e1578435aa195d4ee307
Gabriel Sanches <gabriel@gsr.dev>
2020-02-22 19:25:36 -0300

    cmd/plg: register "list" command
    An E2E test is used to check whether "list" is really registered and
    working properly.

--- cmd/plg/main.go
+++ cmd/plg/main.go
@@ -2,15 +2,51 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/google/subcommands"
+	"gsr.dev/pilgrim/cmd/internal/command"
 )
+
+const defaultConfig = "pilgrim.json"
 
 func main() {
 	os.Exit(run())
 }
 
 func run() int {
-	return int(subcommands.Execute(context.TODO()))
+	exe := os.Args[0]
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", exe, err)
+		return int(subcommands.ExitFailure)
+	}
+	// NOTE: subcommands.NewCommander must be used in order to avoid bugs
+	// when this function is tested using "go-cmdtest".
+	//
+	// The bug consists of having subcommands.DefaultCommand register homonymous
+	// commands more than once for a single ".ct" file.
+	//
+	// Since subcommands.DefaultCommand is global, those commands are appended to the same command
+	// slice and, when subcommands.Execute is called, each command is checked by name within that
+	// command is checked by name within that slice and, since they're homonyms, only the first one
+	// is picked. Because each individual command in a ".ct" file gets new stdout and stderr
+	// redirection in order to be checked, each run writes to writers from the first run.
+	//
+	// This note can be removed when https://github.com/google/subcommands/issues/32 gets resolved.
+	cmd := subcommands.NewCommander(flag.CommandLine, filepath.Base(exe))
+	cmd.Register(command.New(
+		&listCmd{config: defaultConfig, cwd: cwd},
+		command.Name("list"),
+		command.Synopsis("List files to be symlinked."),
+		command.Usage(`list [-check]:
+	List files to be symlinked.`),
+		command.Stdout(os.Stdout),
+		command.Stderr(os.Stderr),
+	), "")
+	flag.Parse()
+	return int(cmd.Execute(context.TODO()))
 }
--- cmd/plg/main_test.go
+++ cmd/plg/main_test.go
@@ -2,18 +2,66 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	cmdtest "github.com/google/go-cmdtest"
 )
 
+const failureStatus = 0xDEADC0DE // 3735929054
+
 var update = flag.Bool("update", false, "update test files with results")
 
 func TestCLI(t *testing.T) {
-	ts, err := cmdtest.Read(filepath.Join("testdata", t.Name()))
+	pwd, err := os.Getwd()
 	if err != nil {
 		t.Fatal(err)
 	}
+	testdata := filepath.Join("testdata", t.Name())
+	ts, err := cmdtest.Read(filepath.Join(testdata, runtime.GOOS))
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Utility commands.
+	ts.Commands["cp"] = cmdtest.InProcessProgram("cp", cpCmd(filepath.Join(pwd, testdata)))
+
+	// Pilgrim commands.
+	ts.Commands["plg"] = cmdtest.InProcessProgram("plg", run)
 	ts.Run(t, *update)
+}
+
+func cpCmd(pwd string) func() int {
+	return func() int {
+		var (
+			argv            = os.Args[1:]
+			argc            = len(argv)
+			original, clone string
+		)
+		if argc > 0 {
+			original = argv[0]
+			if argc == 1 {
+				clone = original
+			} else {
+				clone = argv[1]
+			}
+		}
+		if original == "" || clone == "" {
+			return 0 // NOP
+		}
+		b, err := ioutil.ReadFile(filepath.Join(pwd, original))
+		if err != nil {
+			fmt.Println(err)
+			return failureStatus
+		}
+		if err = ioutil.WriteFile(clone, b, 0o644); err != nil {
+			fmt.Println(err)
+			return failureStatus
+		}
+		return 0
+	}
+
 }
