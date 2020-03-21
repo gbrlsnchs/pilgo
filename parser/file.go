b21d59cfa3a6399b2be445ebdbdcc57ad64a2b62
Gabriel Sanches <gabriel@gsr.dev>
2020-03-20 22:23:20 -0300

    parser: add method to get a file's base dir

--- parser/file.go
+++ parser/file.go
@@ -8,6 +8,20 @@ type File struct {
 	Path    []string
 }
 
+// Dir returns the base directory of a file.
+func (f File) Dir() string {
+	pathc := len(f.Path)
+	// Shortcut for the base directory itself.
+	if pathc < 2 {
+		return f.BaseDir
+	}
+	dir := append(
+		make([]string, 0, len(f.Path)),
+		f.BaseDir,
+	)
+	return filepath.Join(append(dir, f.Path[:pathc-1]...)...)
+}
+
 // FullPath joins base directory with all path segments.
 func (f File) FullPath() string {
 	fullPath := append(
--- parser/file_test.go
+++ parser/file_test.go
@@ -8,7 +8,48 @@ import (
 )
 
 func TestFile(t *testing.T) {
+	t.Run("Dir", testFileDir)
 	t.Run("FullPath", testFileFullPath)
+}
+
+func testFileDir(t *testing.T) {
+	testCases := []struct {
+		f    parser.File
+		want string
+	}{
+		{
+			f:    parser.File{},
+			want: "",
+		},
+		{
+			f: parser.File{
+				BaseDir: "test",
+				Path:    nil,
+			},
+			want: "test",
+		},
+		{
+			f: parser.File{
+				BaseDir: "test",
+				Path:    []string{"foo"},
+			},
+			want: "test",
+		},
+		{
+			f: parser.File{
+				BaseDir: "test",
+				Path:    []string{"foo", "bar"},
+			},
+			want: filepath.Join("test", "foo"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.f.FullPath(), func(t *testing.T) {
+			if want, got := tc.want, tc.f.Dir(); got != want {
+				t.Errorf("want %q, got %q", want, got)
+			}
+		})
+	}
 }
 
 func testFileFullPath(t *testing.T) {
