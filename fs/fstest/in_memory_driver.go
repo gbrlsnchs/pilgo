89b32090eab82580ea076966711dc2abc04c3755
Gabriel Sanches <gabriel@gsr.dev>
2020-03-22 23:52:40 -0300

    fs/fstest: add in-memory driver
    This is a fake file system driver that can be used as a test double to
    make more asserted tests, mainly integration ones.

---
+++ fs/fstest/in_memory_driver.go
@@ -0,0 +1,210 @@
+package fstest
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"gsr.dev/pilgrim/fs"
+)
+
+var (
+	// ErrNotExist means a file doesn't exist.
+	ErrNotExist = errors.New("file doesn't exist")
+	// ErrExist means a file already exists.
+	ErrExist = errors.New("file already exist")
+	// ErrNotDir means a file is not a directory.
+	ErrNotDir = errors.New("file is not a directory")
+
+	pathSep = string(filepath.Separator)
+)
+
+// InMemoryDriver is a synthetic file system that mimics
+// simple behaviors of a real file system in memory.
+type InMemoryDriver struct {
+	Files map[string]File
+}
+
+// MkdirAll simulates the creation of a directory. It also creates the parents of
+// such directory, if needed.
+func (drv *InMemoryDriver) MkdirAll(dirname string) error {
+	_, err := drv.mkdirAll(dirname)
+	return err
+}
+
+// ReadDir simulates a directory read. If returns an error if it can't find dirname.
+func (drv *InMemoryDriver) ReadDir(dirname string) ([]fs.FileInfo, error) {
+	fstat, err := drv.find(dirname)
+	if err != nil {
+		return nil, err
+	}
+	if !fstat.IsDir() {
+		return nil, ErrNotDir
+	}
+	files := fstat.File.Children
+	list := make(sortedFiles, 0, len(files))
+	for p, f := range files {
+		list = append(list, FileStat{p, f})
+	}
+	sort.Sort(list)
+	return list, nil
+}
+
+// ReadFile simulates a file read. It returns data associated with a file or an error
+// if the file can't be found.
+func (drv *InMemoryDriver) ReadFile(filename string) ([]byte, error) {
+	fstat, err := drv.find(filename)
+	if err != nil {
+		return nil, err
+	}
+	return fstat.File.Data, nil
+}
+
+// Stat simulates reading information about filename. If filename doesn't exist, instead of
+// returning an error, Stat returns an empty FileStat object.
+func (drv *InMemoryDriver) Stat(filename string) (fs.FileInfo, error) {
+	fstat, err := drv.find(filename)
+	if errors.Is(err, ErrNotExist) {
+		return FileStat{}, nil
+	}
+	return fstat, err
+}
+
+// Symlink simulates a symlink creation. It creates a symlink if newname doesn't exist,
+// or return an error instead.
+func (drv *InMemoryDriver) Symlink(oldname, newname string) error {
+	f := File{
+		Linkname: oldname,
+		Perm:     os.ModePerm,
+		Data:     nil,
+		Children: nil,
+	}
+	return drv.create(newname, f, true)
+}
+
+// WriteFile simulates a file write by associating data and perm with filename.
+// It only works if filename doesn't already exist and its parent directories, if any, exist.
+// Else, it returns an error.
+func (drv *InMemoryDriver) WriteFile(filename string, data []byte, perm os.FileMode) error {
+	f := File{
+		Linkname: "",
+		Perm:     perm,
+		Data:     data,
+		Children: nil,
+	}
+	return drv.create(filename, f, false)
+}
+
+func (drv *InMemoryDriver) create(filename string, f File, mkdir bool) error {
+	fstat, err := drv.find(filename)
+	if err != nil && !errors.Is(err, ErrNotExist) {
+		return err
+	}
+	if fstat.Exists() {
+		return ErrExist
+	}
+	if drv.Files == nil {
+		drv.Files = make(map[string]File, 1)
+	}
+	parent := drv.Files
+	dir, file := filepath.Split(filename)
+	if dir != "" {
+		fstatFn := drv.find
+		if mkdir {
+			fstatFn = drv.mkdirAll
+		}
+		fstat, err := fstatFn(dir)
+		if err != nil {
+			return err
+		}
+		parent = fstat.File.Children
+	}
+	parent[file] = f
+	return nil
+}
+
+func (drv *InMemoryDriver) find(filename string) (FileStat, error) {
+	var (
+		fstat FileStat
+		files = drv.Files
+		paths = strings.Split(filename, pathSep)
+	)
+	for _, p := range paths {
+		if f, ok := files[p]; ok {
+			files = f.Children
+			fstat = FileStat{p, f}
+			continue
+		}
+		return FileStat{}, fmt.Errorf("fstest: %w", ErrNotExist)
+	}
+	return fstat, nil
+}
+
+func (drv *InMemoryDriver) mkdirAll(dirname string) (FileStat, error) {
+	if drv.Files == nil {
+		drv.Files = make(map[string]File, 1)
+	}
+	var (
+		fstat FileStat
+		files = drv.Files
+		paths = strings.Split(dirname, pathSep)
+	)
+	for _, p := range paths {
+		f, ok := files[p]
+		if ok {
+			fstat := FileStat{p, f}
+			if !fstat.IsDir() {
+				return FileStat{}, ErrExist
+			}
+		} else {
+			f = File{
+				Perm:     os.ModePerm,
+				Linkname: "",
+				Children: make(map[string]File, 0),
+			}
+			files[p] = f
+		}
+		files = f.Children
+		fstat = FileStat{p, f}
+	}
+	return fstat, nil
+}
+
+// File is a representation of a file in a file system.
+type File struct {
+	Perm     os.FileMode
+	Linkname string
+	Data     []byte
+	Children map[string]File
+}
+
+// FileStat is an abstraction of information about a file,
+// which is basically the file itself plus its name.
+type FileStat struct {
+	Label string
+	File  File
+}
+
+// Name returns a file's associated name.
+func (f FileStat) Name() string { return f.Label }
+
+// Exists returns whether a file exists.
+func (f FileStat) Exists() bool { return f.Label != "" }
+
+// IsDir returns whether a file is a directory.
+func (f FileStat) IsDir() bool { return f.File.Children != nil }
+
+// Linkname returns the name of a file a link is pointing to.
+func (f FileStat) Linkname() string { return f.File.Linkname }
+
+// Perm returns a file's associated permission.
+func (f FileStat) Perm() os.FileMode { return f.File.Perm }
+
+type sortedFiles []fs.FileInfo
+
+func (sf sortedFiles) Len() int           { return len(sf) }
+func (sf sortedFiles) Less(i, j int) bool { return sf[i].Name() < sf[j].Name() }
+func (sf sortedFiles) Swap(i, j int)      { sf[i], sf[j] = sf[j], sf[i] }
---
+++ fs/fstest/in_memory_driver_test.go
@@ -0,0 +1,608 @@
+package fstest_test
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"gsr.dev/pilgrim/fs"
+	"gsr.dev/pilgrim/fs/fstest"
+)
+
+var _ fs.Driver = new(fstest.InMemoryDriver)
+
+func TestFakeDriver(t *testing.T) {
+	t.Run("MkdirAll", testFakeDriverMkdirAll)
+	t.Run("ReadDir", testFakeDriverReadDir)
+	t.Run("ReadFile", testFakeDriverReadFile)
+	t.Run("Stat", testFakeDriverStat)
+	t.Run("Symlink", testFakeDriverSymlink)
+	t.Run("WriteFile", testFakeDriverWriteFile)
+}
+
+func testFakeDriverMkdirAll(t *testing.T) {
+	testCases := []struct {
+		drv     fstest.InMemoryDriver
+		dirname string
+		want    fstest.InMemoryDriver
+		err     error
+	}{
+		{
+			drv:     fstest.InMemoryDriver{},
+			dirname: "foo",
+			want: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": fstest.File{
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Children: make(map[string]fstest.File, 0),
+					},
+				},
+			},
+			err: nil,
+		},
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": fstest.File{
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Children: make(map[string]fstest.File, 0),
+					},
+				},
+			},
+			dirname: filepath.Join("foo", "bar"),
+			want: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": fstest.File{
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Children: map[string]fstest.File{
+							"bar": {
+								Linkname: "",
+								Perm:     os.ModePerm,
+								Children: make(map[string]fstest.File, 0),
+							},
+						},
+					},
+				},
+			},
+			err: nil,
+		},
+		{
+			drv:     fstest.InMemoryDriver{},
+			dirname: filepath.Join("foo", "bar"),
+			want: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": fstest.File{
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Children: map[string]fstest.File{
+							"bar": {
+								Linkname: "",
+								Perm:     os.ModePerm,
+								Children: make(map[string]fstest.File, 0),
+							},
+						},
+					},
+				},
+			},
+			err: nil,
+		},
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": fstest.File{
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Children: nil,
+					},
+				},
+			},
+			dirname: filepath.Join("foo", "bar"),
+			want: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": fstest.File{
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Children: nil,
+					},
+				},
+			},
+			err: fstest.ErrExist,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.dirname, func(t *testing.T) {
+			err := tc.drv.MkdirAll(tc.dirname)
+			if want, got := tc.err, err; !errors.Is(got, want) {
+				t.Fatalf("want %v, got %v", want, got)
+			}
+			if want, got := tc.want, tc.drv; !cmp.Equal(got, want) {
+				t.Fatalf("(-want +got):\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
+
+func testFakeDriverReadDir(t *testing.T) {
+	testCases := []struct {
+		drv     fstest.InMemoryDriver
+		dirname string
+		want    []fs.FileInfo
+		err     error
+	}{
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"test": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     nil,
+						Children: map[string]fstest.File{
+							"bar": {
+								Linkname: "foo",
+								Perm:     os.ModePerm,
+								Data:     nil,
+								Children: nil,
+							},
+							"foo": {
+								Linkname: "",
+								Perm:     os.ModePerm,
+								Data:     []byte("test_foo"),
+								Children: nil,
+							},
+						},
+					},
+				},
+			},
+			dirname: "test",
+			want: []fs.FileInfo{
+				fstest.FileStat{
+					Label: "bar",
+					File: fstest.File{
+						Linkname: "foo",
+						Perm:     os.ModePerm,
+						Data:     nil,
+						Children: nil,
+					},
+				},
+				fstest.FileStat{
+					Label: "foo",
+					File: fstest.File{
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test_foo"),
+						Children: nil,
+					},
+				},
+			},
+			err: nil,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.dirname, func(t *testing.T) {
+			orig := tc.drv // copy for further comparison
+			files, err := tc.drv.ReadDir(tc.dirname)
+			if want, got := tc.err, err; !errors.Is(got, want) {
+				t.Fatalf("want %v, got %v", want, got)
+			}
+			if want, got := tc.want, files; !cmp.Equal(got, want) {
+				t.Fatalf("(-want +got):\n%s", cmp.Diff(want, got))
+			}
+			if want, got := orig, tc.drv; !cmp.Equal(got, want) {
+				t.Fatalf("(-want +got):\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
+
+func testFakeDriverReadFile(t *testing.T) {
+	testCases := []struct {
+		drv      fstest.InMemoryDriver
+		filename string
+		want     []byte
+		err      error
+	}{
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test"),
+						Children: nil,
+					},
+				},
+			},
+			filename: "foo",
+			want:     []byte("test"),
+			err:      nil,
+		},
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test_foo"),
+						Children: map[string]fstest.File{
+							"bar": {
+								Linkname: "",
+								Perm:     os.ModePerm,
+								Data:     []byte("test_bar"),
+							},
+						},
+					},
+				},
+			},
+			filename: filepath.Join("foo", "bar"),
+			want:     []byte("test_bar"),
+			err:      nil,
+		},
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test"),
+						Children: nil,
+					},
+				},
+			},
+			filename: "bar",
+			want:     nil,
+			err:      fstest.ErrNotExist,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.filename, func(t *testing.T) {
+			orig := tc.drv // copy for further comparison
+			b, err := tc.drv.ReadFile(tc.filename)
+			if want, got := tc.err, err; !errors.Is(got, want) {
+				t.Fatalf("want %v, got %v", want, got)
+			}
+			if want, got := tc.want, b; string(got) != string(want) {
+				t.Fatalf("want %q, got %q", want, got)
+			}
+			if want, got := orig, tc.drv; !cmp.Equal(got, want) {
+				t.Fatalf("(-want +got):\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
+
+func testFakeDriverStat(t *testing.T) {
+	testCases := []struct {
+		drv      fstest.InMemoryDriver
+		filename string
+		want     fstest.FileStat
+		err      error
+	}{
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test"),
+						Children: nil,
+					},
+				},
+			},
+			filename: "foo",
+			want: fstest.FileStat{
+				Label: "foo",
+				File: fstest.File{
+					Linkname: "",
+					Perm:     os.ModePerm,
+					Data:     []byte("test"),
+					Children: nil,
+				},
+			},
+			err: nil,
+		},
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test_foo"),
+						Children: map[string]fstest.File{
+							"bar": {
+								Linkname: "",
+								Perm:     os.ModePerm,
+								Data:     []byte("test_bar"),
+							},
+						},
+					},
+				},
+			},
+			filename: filepath.Join("foo", "bar"),
+			want: fstest.FileStat{
+				Label: "bar",
+				File: fstest.File{
+					Linkname: "",
+					Perm:     os.ModePerm,
+					Data:     []byte("test_bar"),
+				},
+			},
+			err: nil,
+		},
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test"),
+						Children: nil,
+					},
+				},
+			},
+			filename: "bar",
+			want:     fstest.FileStat{},
+			err:      nil,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.filename, func(t *testing.T) {
+			t.Run(tc.filename, func(t *testing.T) {
+				orig := tc.drv // copy for further comparison
+				b, err := tc.drv.Stat(tc.filename)
+				if want, got := tc.err, err; !errors.Is(got, want) {
+					t.Fatalf("want %v, got %v", want, got)
+				}
+				if want, got := tc.want, b; !cmp.Equal(got, want) {
+					t.Fatalf("(-want +got):\n%s", cmp.Diff(want, got))
+				}
+				if want, got := orig, tc.drv; !cmp.Equal(got, want) {
+					t.Fatalf("(-want +got):\n%s", cmp.Diff(want, got))
+				}
+			})
+		})
+	}
+}
+
+func testFakeDriverSymlink(t *testing.T) {
+	testCases := []struct {
+		drv     fstest.InMemoryDriver
+		oldname string
+		newname string
+		want    fstest.InMemoryDriver
+		err     error
+	}{
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test"),
+						Children: nil,
+					},
+				},
+			},
+			oldname: "foo",
+			newname: "bar",
+			want: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"bar": {
+						Linkname: "foo",
+						Perm:     os.ModePerm,
+						Data:     nil,
+						Children: nil,
+					},
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test"),
+						Children: nil,
+					},
+				},
+			},
+			err: nil,
+		},
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"bar": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test_bar"),
+						Children: nil,
+					},
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test_foo"),
+						Children: nil,
+					},
+				},
+			},
+			oldname: "foo",
+			newname: "bar",
+			want: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"bar": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test_bar"),
+						Children: nil,
+					},
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     []byte("test_foo"),
+						Children: nil,
+					},
+				},
+			},
+			err: fstest.ErrExist,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.oldname+" "+tc.newname, func(t *testing.T) {
+			err := tc.drv.Symlink(tc.oldname, tc.newname)
+			if want, got := tc.err, err; !errors.Is(got, want) {
+				t.Fatalf("want %v, got %v", want, got)
+			}
+			if want, got := tc.want, tc.drv; !cmp.Equal(got, want) {
+				t.Fatalf("(-want +got):\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
+
+func testFakeDriverWriteFile(t *testing.T) {
+	testCases := []struct {
+		drv      fstest.InMemoryDriver
+		filename string
+		data     []byte
+		perm     os.FileMode
+		want     fstest.InMemoryDriver
+		err      error
+	}{
+		{
+			drv:      fstest.InMemoryDriver{},
+			filename: "foo",
+			data:     []byte("bar"),
+			perm:     0o644,
+			want: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     0o644,
+						Data:     []byte("bar"),
+						Children: nil,
+					},
+				},
+			},
+			err: nil,
+		},
+		{
+			drv: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     nil,
+						Children: nil,
+					},
+				},
+			},
+			filename: "foo",
+			data:     []byte("bar"),
+			perm:     0o644,
+			want: fstest.InMemoryDriver{
+				Files: map[string]fstest.File{
+					"foo": {
+						Linkname: "",
+						Perm:     os.ModePerm,
+						Data:     nil,
+						Children: nil,
+					},
+				},
+			},
+			err: fstest.ErrExist,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.filename, func(t *testing.T) {
+			err := tc.drv.WriteFile(tc.filename, tc.data, tc.perm)
+			if want, got := tc.err, err; !errors.Is(got, want) {
+				t.Fatalf("want %v, got %v", want, got)
+			}
+			if want, got := tc.want, tc.drv; !cmp.Equal(got, want) {
+				t.Fatalf("(-want +got):\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
+
+func TestFileStat(t *testing.T) {
+	t.Run("Name", func(t *testing.T) {
+		testCases := []struct {
+			f    fstest.FileStat
+			want string
+		}{
+			{fstest.FileStat{Label: "foo", File: fstest.File{}}, "foo"},
+		}
+		for _, tc := range testCases {
+			t.Run("", func(t *testing.T) {
+				if want, got := tc.want, tc.f.Name(); got != want {
+					t.Fatalf("want %q, got %q", want, got)
+				}
+			})
+		}
+	})
+	t.Run("Exists", func(t *testing.T) {
+		testCases := []struct {
+			f    fstest.FileStat
+			want bool
+		}{
+			{fstest.FileStat{Label: "foo", File: fstest.File{}}, true},
+			{fstest.FileStat{}, false},
+		}
+		for _, tc := range testCases {
+			t.Run("", func(t *testing.T) {
+				if want, got := tc.want, tc.f.Exists(); got != want {
+					t.Fatalf("want %t, got %t", want, got)
+				}
+			})
+		}
+	})
+	t.Run("IsDir", func(t *testing.T) {
+		testCases := []struct {
+			f    fstest.FileStat
+			want bool
+		}{
+			{fstest.FileStat{File: fstest.File{Children: nil}}, false},
+			{fstest.FileStat{File: fstest.File{Children: make(map[string]fstest.File, 0)}}, true},
+			{fstest.FileStat{File: fstest.File{Children: make(map[string]fstest.File, 5)}}, true},
+		}
+		for _, tc := range testCases {
+			t.Run("", func(t *testing.T) {
+				if want, got := tc.want, tc.f.IsDir(); got != want {
+					t.Fatalf("want %t, got %t", want, got)
+				}
+			})
+		}
+	})
+	t.Run("Linkname", func(t *testing.T) {
+		testCases := []struct {
+			f    fstest.FileStat
+			want string
+		}{
+			{
+				fstest.FileStat{File: fstest.File{Linkname: "foo"}}, "foo",
+			},
+		}
+		for _, tc := range testCases {
+			t.Run("", func(t *testing.T) {
+				if want, got := tc.want, tc.f.Linkname(); got != want {
+					t.Fatalf("want %q, got %q", want, got)
+				}
+			})
+		}
+	})
+	t.Run("Perm", func(t *testing.T) {
+		testCases := []struct {
+			f    fstest.FileStat
+			want os.FileMode
+		}{
+			{fstest.FileStat{File: fstest.File{Perm: os.ModePerm}}, os.ModePerm},
+		}
+		for _, tc := range testCases {
+			t.Run("", func(t *testing.T) {
+				if want, got := tc.want, tc.f.Perm(); got != want {
+					t.Fatalf("want %#o, got %#o", got, want)
+				}
+			})
+		}
+	})
+}
