41a91d5326299b2a55b9f84e1060fe3912f83832
Gabriel Sanches <gabriel@gsr.dev>
2020-04-24 00:58:55 -0300

    add Mage target to gen E2E tests based on Linux's
    By generating the tests for macOS and Windows, we only need to write
    once (per test). This awesome Mage target deals with text substitutions
    in other to port tests correctly.

--- magefile.go
+++ magefile.go
@@ -3,11 +3,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/google/renameio"
 	"github.com/magefile/mage/sh"
 )
 
@@ -15,6 +18,7 @@ const (
 	module         = "github.com/gbrlsnchs/pilgo"
 	versionEnvName = "PILGO_VERSION"
 	dstdir         = ".bin"
+	sep            = string(filepath.Separator)
 )
 
 var (
@@ -46,6 +50,63 @@ func Build() error {
 				return err
 			}
 
+		}
+	}
+	return nil
+}
+
+type linuxTxt string
+
+const (
+	slash        linuxTxt = "/"
+	fileNotFound linuxTxt = "no such file or directory"
+	readDirErr   linuxTxt = "readdirent: not a directory"
+)
+
+type replacement struct {
+	old linuxTxt
+	new string
+}
+
+// GenCLITests copies Linux E2E CLI tests and properly adapts it to other platforms.
+func GenCLITests() error {
+	const linuxdir = "linux"
+	oses := map[string][]replacement{
+		"darwin": {
+			{old: readDirErr, new: "fdopendir: not a directory"},
+		},
+		"windows": {
+			{old: slash, new: "\\"},
+			{old: fileNotFound, new: "The system cannot find the file specified."},
+		},
+	}
+	for sys, rep := range oses {
+		testdir := filepath.Join("cmd", "plg", "testdata", "TestCLI")
+		windir := filepath.Join(testdir, sys)
+		if err := os.RemoveAll(windir); err != nil {
+			return err
+		}
+		if err := os.Mkdir(windir, 0o755); err != nil {
+			return err
+		}
+		srcdir := filepath.Join(testdir, linuxdir)
+		files, err := ioutil.ReadDir(filepath.Join(testdir, linuxdir))
+		if err != nil {
+			return err
+		}
+		for _, fi := range files {
+			fname := fi.Name()
+			b, err := ioutil.ReadFile(filepath.Join(srcdir, fname))
+			if err != nil {
+				return err
+			}
+			for _, r := range rep {
+				b = bytes.ReplaceAll(b, []byte(r.old), []byte(r.new))
+			}
+			if err := renameio.WriteFile(filepath.Join(windir, fname),
+				b, fi.Mode().Perm()); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
